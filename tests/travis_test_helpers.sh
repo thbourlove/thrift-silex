6c3a4f5533cb8064025eca45e07349c4a8b416c7
thbourlove <thbourlove@gmail.com>
2014-06-20 20:13:27 +0800

    for test_helpers install

--- tests/travis_test_helpers.sh
+++ tests/travis_test_helpers.sh
@@ -1,4 +1,3 @@
 wget https://github.com/php-test-helpers/php-test-helpers/tarball/master
 tar -zxf master
 sh -c "cd php-test-helpers-php-test-helpers-*&&phpize; ./configure; make; sudo make install"
-phpenv config-add test_helpers.ini
