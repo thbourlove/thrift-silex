99f358a3962691eb90196ca0ba455ab91aa24dd4
thbourlove <thbourlove@gmail.com>
2014-06-20 20:10:18 +0800

    fix order of test_helpers

--- tests/travis_test_helpers.sh
+++ tests/travis_test_helpers.sh
@@ -1,4 +1,4 @@
 wget https://github.com/php-test-helpers/php-test-helpers/tarball/master
 tar -zxf master
 sh -c "cd php-test-helpers-php-test-helpers-*&&phpize; ./configure; make; sudo make install"
-echo "extension=test_helpers.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+phpenv config-add test_helpers.ini
